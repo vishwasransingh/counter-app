a48c9d2c2fb4a8ac1a6f8c58f3b25bd65be9dd0e
vishwasransing <vishwasransing@gmail.com>
2024-01-08 10:55:48 +0530

    Props demo 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Counter from './components/counter/Counter';
 
-function App() {
+function App() { 
   return (
     <div className="App">
+      <DemoProps property1="Value1" property2="Value2"/>
       <Counter/>
     </div>
   );
+}
+
+function DemoProps(properties) {
+  return(
+    <div>{properties.property1} {properties.property2}</div>
+  )
 }
 
 export default App;
