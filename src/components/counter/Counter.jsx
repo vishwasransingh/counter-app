b65d9e7582f826a77563e6870f91781475ab4332
vishwasransing <vishwasransing@gmail.com>
2024-01-08 10:34:00 +0530

    Created counter with multiple buttons.
    -Added CSS

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,14 +1,48 @@
+import './Counter.css';
+import { useState } from 'react';
+
 export default function Counter() {
 
+    const [count, setCount] = useState(0)
+
     function incrementCounterFunction() {
-        console.log('increment clicked')
+        setCount(count + 1);
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - 1);
+    }
+
+    function incrementCounterBy2Function(){
+        setCount(count + 2);
+    }
+
+    function decrementCounterBy2Function() {
+        setCount(count - 2);
+    }
+
+    function incrementCounterBy5Function() {
+        setCount(count + 3);
+    }
+
+    function decrementCounterBy5Function() {
+        setCount(count -3);
     }
 
     return(
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
+                <button className='counterButton' onClick={decrementCounterFunction}>-1</button>
+            </div>
+            <div>
+                <button className="counterButton" onClick={incrementCounterBy2Function}>+2</button>
+                <button className="counterButton" onClick={decrementCounterBy2Function}>-2</button>
+            </div>
+            <div>
+                <button className="counterButton" onClick={incrementCounterBy5Function}>+5</button>
+                <button className="counterButton" onClick={decrementCounterBy5Function}>-5</button>
             </div>
         </div>
         )
